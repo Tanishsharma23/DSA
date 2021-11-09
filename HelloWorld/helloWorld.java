ae9b1275eb6fb7fb0b8cd11b854d1909009e8340
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-11-09 23:56:55 +0530

    Add files via upload

---
+++ HelloWorld/helloWorld.java
@@ -0,0 +1,23 @@
+package HelloWorld;
+
+public class helloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int [] arr1 = new int[3];
+		
+		
+		arr1[0]=20;
+		arr1[1]=20;
+		arr1[2]=20;
+		
+		for(int i = 0 ;i<=arr1.length-1;i++) {
+			System.out.println(arr1[i]);
+		}
+		
+			System.out.println("Hello Wolrd");
+		
+	}
+
+}
