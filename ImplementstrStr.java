c6679dc668478ae2c191742416f4000b079ed49c
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-11-10 23:53:09 +0530

    Add files via upload

---
+++ ImplementstrStr.java
@@ -0,0 +1,14 @@
+package Array;
+
+public class ImplementstrStr {
+
+	
+}
+
+class Solution {
+    public int strStr(String haystack, String needle) {
+            
+            return haystack.indexOf(needle); //will return very first index (if needle is present in haystack)
+        
+    }
+}
