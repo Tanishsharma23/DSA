7e4531c7fdf3db626886e38578623f1fbe09cc8f
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-11-20 20:27:04 +0530

    Add files via upload

---
+++ ReverseVowelsOfString.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class ReverseVowelsOfString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s = "hello";
+		System.out.println(reverseVowels(s));
+
+	}
+	
+	 public static String reverseVowels(String s) {
+	      
+	      ArrayList<Character> vowels = new ArrayList<>();
+	        vowels.add('a');
+	        vowels.add('e');
+	        vowels.add('i');
+	        vowels.add('o');
+	        vowels.add('u');
+	        vowels.add('A');
+	        vowels.add('E');
+	        vowels.add('I');
+	        vowels.add('O');
+	        vowels.add('U'); 
+	        
+	        char [] ques = s.toCharArray();
+	        
+	        int left = 0;
+	        int right = ques.length - 1;
+	        
+	        while(right > left){
+	            
+	            if(!vowels.contains(ques[left])){left++;}
+	            if(!vowels.contains(ques[right])){right--;}
+	           
+	            
+	            if(vowels.contains(ques[left]) && vowels.contains(ques[right] ) ){
+	                
+	                char temp = ques[left];
+	                ques[left] = ques[right];
+	                ques[right] = temp;
+	                
+	                left ++ ;
+	                right --;
+	            } 
+	            
+	        
+	        String ans = String.valueOf(ques); 
+	        
+
+	     
+	        return ans;
+	     
+	        
+	        
+	        
+	    }
+
+}
