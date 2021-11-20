98c6329cd441d5a32f8e7d1fb06c4533ecff5a2b
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-11-21 04:09:15 +0530

    Add files via upload

--- ReverseVowelsOfString.java
+++ ReverseVowelsOfString.java
@@ -10,52 +10,52 @@ public class ReverseVowelsOfString {
 		System.out.println(reverseVowels(s));
 
 	}
-	
-	 public static String reverseVowels(String s) {
-	      
-	      ArrayList<Character> vowels = new ArrayList<>();
-	        vowels.add('a');
-	        vowels.add('e');
-	        vowels.add('i');
-	        vowels.add('o');
-	        vowels.add('u');
-	        vowels.add('A');
-	        vowels.add('E');
-	        vowels.add('I');
-	        vowels.add('O');
-	        vowels.add('U'); 
-	        
-	        char [] ques = s.toCharArray();
-	        
-	        int left = 0;
-	        int right = ques.length - 1;
-	        
-	        while(right > left){
-	            
-	            if(!vowels.contains(ques[left])){left++;}
-	            if(!vowels.contains(ques[right])){right--;}
-	           
-	            
-	            if(vowels.contains(ques[left]) && vowels.contains(ques[right] ) ){
-	                
-	                char temp = ques[left];
-	                ques[left] = ques[right];
-	                ques[right] = temp;
-	                
-	                left ++ ;
-	                right --;
-	            } 
-	            
-	        
-	        String ans = String.valueOf(ques); 
-	        
 
-	     
-	        return ans;
-	     
-	        
-	        
-	        
-	    }
+	public static String reverseVowels(String s) {
 
+		ArrayList<Character> vowels = new ArrayList<>();
+		vowels.add('a');
+		vowels.add('e');
+		vowels.add('i');
+		vowels.add('o');
+		vowels.add('u');
+		vowels.add('A');
+		vowels.add('E');
+		vowels.add('I');
+		vowels.add('O');
+		vowels.add('U');
+
+		char[] ques = s.toCharArray();
+
+		int left = 0;
+		int right = ques.length - 1;
+
+		while (right > left) {
+
+			if (!vowels.contains(ques[left])) {
+				left++;
+			}
+			if (!vowels.contains(ques[right])) {
+				right--;
+			}
+
+			if (vowels.contains(ques[left]) && vowels.contains(ques[right])) {
+
+				char temp = ques[left];
+				ques[left] = ques[right];
+				ques[right] = temp;
+
+				left++;
+				right--;
+			}
+
+			
+
+			
+
+		}
+		String ans = String.valueOf(ques);
+		return ans;
+
+	}
 }
