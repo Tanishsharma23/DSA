1283e3b4ca273d80a1b27ba987f794425dcfd57d
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-11-22 22:52:47 +0530

    Add files via upload

---
+++ backTraacking.java
@@ -0,0 +1,24 @@
+
+public class backTraacking {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = new int[6];
+		print(arr, 0);
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + " ");
+		}
+
+	}
+
+	public static void print(int[] arr, int i) {
+		if (i == arr.length) {
+			return;
+		}
+
+		arr[i] = 10;
+		print(arr, i + 1);
+		arr[i] = 0;
+	}
+
+}
