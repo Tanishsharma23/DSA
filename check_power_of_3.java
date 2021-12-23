188744a019585fa406427dd65d1435faf7c8ff07
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2021-12-23 23:18:58 +0530

    Add files via upload

---
+++ check_power_of_3.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class check_power_of_3 {
+	public boolean checkPowersOfThree(int n) {
+
+		if (n == 1) {
+			return true;
+		}
+		if (n % 3 == 0) {
+
+			return checkPowersOfThree(n / 3);
+
+		}
+
+		if (n % 3 == 1) {
+			return checkPowersOfThree((n - 1) / 3);
+		}
+		return false;
+
+	}
+
+}
