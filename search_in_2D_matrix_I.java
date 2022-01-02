7b01a54835821f9ef96be024155e7525268b4a7e
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-01-02 22:36:37 +0530

    Add files via upload

---
+++ search_in_2D_matrix_I.java
@@ -0,0 +1,21 @@
+package leetcode;
+
+public class search_in_2D_matrix_I {
+	public boolean searchMatrix(int[][] matrix, int target) {
+
+		int row = 0;
+		int col = matrix[0].length - 1;
+
+		while (row <= matrix.length - 1 && col >= 0) {
+			if (matrix[row][col] == target) {
+				return true;
+			} else if (matrix[row][col] < target) {
+				row++;
+			} else {
+				col--;
+			}
+		}
+		return false;
+
+	}
+}
