41bd79e2c3678397039b46aee4d1305c97894a91
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-01-07 23:18:37 +0530

    Add files via upload

---
+++ divide_two_int.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+public class divide_two_int {
+	public int divide(int dividend, int divisor) {
+		if (dividend == -2147483648 && divisor == -1) {
+			return 2147483647;
+		}
+		return dividend / divisor;
+	}
+}
