36f4daeb07a7460da5b5aa18de65124992e091a3
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-02-01 23:30:48 +0530

    Add files via upload

---
+++ Find_The_Duplicate_Number_.java
@@ -0,0 +1,22 @@
+package leetcode;
+
+public class Find_The_Duplicate_Number_ {
+	public int findDuplicate(int[] nums) {
+
+		int slow = nums[0];
+		int fast = nums[0];
+		do {
+			slow = nums[slow];
+			fast = nums[nums[fast]];
+		} while (slow != fast);
+
+		fast = nums[0];
+
+		while (slow != fast) {
+			fast = nums[fast];
+			slow = nums[slow];
+		}
+		return slow;
+
+	}
+}
