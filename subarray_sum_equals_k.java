ad4ef729f9013bdbe4d4cf41e38b7370e64e2c67
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-02-10 22:47:19 +0530

    Add files via upload

---
+++ subarray_sum_equals_k.java
@@ -0,0 +1,28 @@
+package leetcode;
+import java.util.*;
+public class subarray_sum_equals_k {
+	public int subarraySum(int[] nums, int k) {
+
+		HashMap<Integer, Integer> map = new HashMap<>();
+		map.put(0, 1);
+
+		int sum = 0;
+		int ans = 0;
+		for (int i = 0; i < nums.length; i++) {
+
+			sum += nums[i];
+
+			if (map.containsKey(sum - k)) {
+				ans += map.get(sum - k);
+			}
+			if (map.containsKey(sum)) {
+				map.put(sum, map.get(sum) + 1);
+			} else {
+				map.put(sum, 1);
+			}
+
+		}
+		return ans;
+
+	}
+}
