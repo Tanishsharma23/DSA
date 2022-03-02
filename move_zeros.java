d9e6eaf01527a3fb3151ad060c1959c6cdd16698
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-03-02 23:49:51 +0530

    Add files via upload

---
+++ move_zeros.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+public class move_zeros {
+	public void moveZeroes(int[] nums) {
+
+		int idx = 0;
+		for (int i = 0; i < nums.length; i++) {
+			if (nums[i] != 0) {
+				nums[idx++] = nums[i];
+			}
+		}
+
+		while (idx < nums.length) {
+			nums[idx++] = 0;
+
+		}
+
+	}
+}
