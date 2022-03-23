b05533973c582b287acff7a4cb17ce76ff277164
Tanish Sharma <84978157+Tanishsharma23@users.noreply.github.com>
2022-03-23 23:53:01 +0530

    Add files via upload

---
+++ Broken_Calculator.java
@@ -0,0 +1,29 @@
+package leetcode;
+
+public class Broken_Calculator {
+
+	public int brokenCalc(int startValue, int target) {
+
+		int steps = 0;
+
+		while (target > startValue) {
+
+			if (target % 2 == 0) {
+
+				target = target / 2;
+
+			} else {
+
+				target++;
+
+			}
+
+			steps++;
+
+		}
+
+		return steps + (startValue - target);
+
+	}
+
+}
